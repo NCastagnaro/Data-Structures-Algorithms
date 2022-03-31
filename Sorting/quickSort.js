9642a3f055e3743cbb10c45d806fa304d0429fbb
Nick Castagnaro <nickshs123@gmail.com>
2022-03-31 16:56:58 -0400

    Update quickSort.js

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -11,16 +11,17 @@ const pivHelper = (arr, start = 0, end = arr.length - 1) => {
 
     for(let i = start + 1; i <= end; i++){
         if(pivot > arr[i]){
-            swapIndex++;
+            swapIndex++;                     //increments each time pivot > arr[i] 
             swap(arr,i,swapIndex);
         }
     }
     swap(arr,start,swapIndex);
     console.log(arr)
-    return swapIndex;
+    return swapIndex;                       //Return swapIndex and this value will be utilized with the recursive quickSort function. 
 
 }
-
+//The quickSort function recursively sorts the array through the constant use of the pivot helper function. 
+//The swapIndex that gets returned is assigned to pivotIndex and is used in the quickSort function
 const quickSort = (arr,left = 0, right = arr.length -1) => {
     if(left < right){
         let pivotIndex = pivHelper(arr,left,right);
